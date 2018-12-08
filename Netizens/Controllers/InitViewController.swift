c28098f01fadc9378a2f6ac455309802612bd1cd
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-12-08 16:32:03 +0100

    Folders hierarchy

--- Netizens/Controllers/InitViewController.swift
+++ Netizens/Controllers/InitViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  InitViewController.swift
 //  Netizens
 //
 //  Created by Krzysztof Lech on 08/12/2018.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InitViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
-
-
 }
-
---
+++ Netizens/Other/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Netizens
+//
+//  Created by Krzysztof Lech on 08/12/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let itemsUrl = "https://jsonplaceholder.typicode.com/photos"
+}
