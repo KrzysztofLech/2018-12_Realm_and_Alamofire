966f307a2a4a04393395b7dd1cc3a8f51d28e252
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-12-08 16:51:30 +0100

    PhotoItem model added

---
+++ Netizens/Model/PhotoItem.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoItem.swift
+//  Netizens
+//
+//  Created by Krzysztof Lech on 08/12/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoItem: Codable {
+    let title: String
+    let thumbnailUrl: String
+}
