d4d8ce7d03841d32044854116cab336bd41cb64f
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-12-08 17:37:52 +0100

    ListViewController added

---
+++ Netizens/Controllers/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  Netizens
+//
+//  Created by Krzysztof Lech on 08/12/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
