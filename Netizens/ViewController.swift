c57dc0a3680a35ad882103786acc1a63e7abc509
Krzysztof Lech <krzysztof.lech@24abc.pl>
2018-12-08 16:08:25 +0100

    Initial Commit

---
+++ Netizens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Netizens
+//
+//  Created by Krzysztof Lech on 08/12/2018.
+//  Copyright © 2018 Krzysztof Lech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
